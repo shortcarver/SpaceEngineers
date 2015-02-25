3d78ce41cfb8848b91b99e3d39a5bd9f119c5ade
Michael Carver <michael.j.carver@gmail.com>
2015-02-24 21:21:46 -0600

    Cleaned up the cargo script a bit.

--- InventoryManagement/InventoryManagement/CargoPercent.cs
+++ InventoryManagement/InventoryManagement/CargoPercent.cs
@@ -18,11 +18,15 @@ namespace InventoryManagement
 
         #region CodeEditor
         String lcdname = "Cargo Status";
+        String trashContainerName = "trash";
+
         List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
         IMyTextPanel lcd;
 
         long maxvol = 0;
         long curvol = 0;
+        long trashmaxvol = 0;
+        long trashcurvol = 0;
 
         void Main()
         {
@@ -32,19 +36,34 @@ namespace InventoryManagement
             {
                 IMyCargoContainer container = (IMyCargoContainer)containers[i];
 
-                maxvol += (long)container.GetInventory(0).MaxVolume;
-                curvol += (long)container.GetInventory(0).CurrentVolume;
+                // count trash
+                if (container.DisplayNameText.ToLower().Contains(trashContainerName))
+                {
+                    trashmaxvol += (long)container.GetInventory(0).MaxVolume;
+                    trashcurvol += (long)container.GetInventory(0).CurrentVolume;
+                }
+                else
+                {
+                    maxvol += (long)container.GetInventory(0).MaxVolume;
+                    curvol += (long)container.GetInventory(0).CurrentVolume;
+                }
             }
             if (maxvol > 0)
             {
-                lcd.WritePublicText("Containers: " + ((curvol * 100) / maxvol) + "%", false);
+                lcd.WritePublicText("Storage: " + ((curvol * 100) / maxvol) + "%", false);
             }
             else
             {
-                lcd.WritePublicText("Containers: 0%", false);
+                lcd.WritePublicText("Storage: 0%", false);
             }
-
-            //lcd.ApplyAction("OnOff_Off");
+            if (trashmaxvol > 0)
+            {
+                lcd.WritePublicText("Trash: " + ((trashcurvol * 100) / trashmaxvol) + "%", true);
+            }
+            else
+            {
+                lcd.WritePublicText("Trash: 0%", true);
+            }
         }
 
         void initBlocks()
