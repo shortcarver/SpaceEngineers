158cc7172fa6b234eb6bf013420f05884bdd7328
Michael Carver <michael.j.carver@gmail.com>
2015-03-14 10:33:17 -0500

    Added a script to auto adjust solar panels on rotors.. but I can't figure out how to actually control the rotors.
    Removed the 'trash' code from CargoPercent.cs since the new sorter blocks make this irrelevant.

--- InventoryManagement/InventoryManagement/CargoPercent.cs
+++ InventoryManagement/InventoryManagement/CargoPercent.cs
@@ -18,56 +18,36 @@ namespace InventoryManagement
 
         #region CodeEditor
         String lcdname = "Cargo Status";
-        String trashContainerName = "trash";
 
         List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
         IMyTextPanel lcd;
 
         long maxvol = 0;
         long curvol = 0;
-        long trashmaxvol = 0;
-        long trashcurvol = 0;
 
         void Main()
         {
             maxvol = 0;
             curvol = 0;
-            trashmaxvol = 0;
-            trashcurvol = 0;
             initBlocks();
 
             for (int i = 0; i < containers.Count; i++)
             {
                 IMyCargoContainer container = (IMyCargoContainer)containers[i];
 
-                // count trash
-                if (container.DisplayNameText.ToLower().Contains(trashContainerName))
-                {
-                    trashmaxvol += (long)container.GetInventory(0).MaxVolume;
-                    trashcurvol += (long)container.GetInventory(0).CurrentVolume;
-                }
-                else
-                {
                     maxvol += (long)container.GetInventory(0).MaxVolume;
                     curvol += (long)container.GetInventory(0).CurrentVolume;
-                }
+
             }
             if (maxvol > 0)
             {
-                lcd.WritePublicText("Storage: " + ((curvol * 100) / maxvol) + "%\n", false);
+                lcd.WritePublicText("Storage: " + ((curvol * 100) / maxvol) + "%", false);
             }
             else
             {
-                lcd.WritePublicText("Storage: 0%\n", false);
+                lcd.WritePublicText("Storage: 0%", false);
             }
-            if (trashmaxvol > 0)
-            {
-                lcd.WritePublicText("Trash: " + ((trashcurvol * 100) / trashmaxvol) + "%", true);
-            }
-            else
-            {
-                lcd.WritePublicText("Trash: 0%", true);
-            }
+
         }
 
         void initBlocks()
